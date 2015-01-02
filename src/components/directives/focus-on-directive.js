65e82830cb34d2fa0e20cf24283f0b7ee26cbebe
Chris Davies <chris.davies@theironyard.com>
2015-01-02 17:39:40 -0500

    Adding a focus directive

---
+++ src/components/directives/focus-on-directive.js
@@ -0,0 +1,7 @@
+app.module('directives').directive('focusOn', function() {
+  return function($scope, elem, attr) {
+    $scope.$on(attr.focusOn, function(e) {
+      elem[0].focus();
+    });
+  };
+});
