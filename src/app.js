09da47ca12c5b528b318615b79e64c5f6b820dd9
Chris Davies <chris.davies@theironyard.com>
2015-01-03 05:10:51 -0500

    Fixing module definitions in app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 var app = (function () {
-  var dependencies = [
-    'ngRoute',
-    'ngResource'
-  ];
+  var registeredModules = {
+    'ngRoute': true,
+    'ngResource': true
+  };
 
   return {
     // Registers an angular module and auto-injects it as a dependency of app
     module: function (name) {
-      dependencies.push(name);
+      // If the module has already been defined, return it
+      if (registeredModules[name]) {
+        return angular.module(name);
+      }
+
+      // Otherwise, define it and return it
+      registeredModules[name] = true;
       return angular.module(name, []);
     },
 
     // This should run after all angular modules have been declared
     initialize: function () {
-      angular.module('app', dependencies)
+      angular.module('app', Object.keys(registeredModules))
         .config(function ($routeProvider) {
           $routeProvider.otherwise({
             controller: 'Error404Ctrl',
